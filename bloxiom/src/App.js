421a65691da3eefe2e5c5206203c2013e6785bd1
mshowman <mshowman@xogrp.com>
2019-10-08 17:54:02 -0400

    working on rule setting component

--- bloxiom/src/App.js
+++ bloxiom/src/App.js
@@ -1,48 +1,88 @@
-import React from 'react';
-import { Select } from '@material-ui/core';
+import React from "react";
+import { Select } from "@material-ui/core";
 import Input from "@material-ui/core/Input";
 import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const groupMockValues = [
-  'Entire Org',
-  'Group 1',
-  'Group 2',
-  'Group N',
-];
+const groupMockValues = ["Entire Org", "Group 1", "Group 2", "Group N"];
+
+const individualMockValues = ["Person 1", "Person 2", "Person 3", "Person N"];
+
+const useStyles = makeStyles({
+  root: {
+    margin: 20,
+    display: "flex",
+    flexDirection: "row"
+  },
+  select: {
+    width: 300
+  },
+  label: {
+    marginBottom: 10,
+    marginLeft: 10
+  },
+  container: {
+    flexDirection: "column"
+  }
+});
 
 function App() {
-  const [selectedGroups, setSelectedGroups] = React.useState([]);
+  const classes = useStyles();
 
-  const handleChange = (event) => {
-    setSelectedGroups(event.target.value);
+  const [selectedGroup, setSelectedGroup] = React.useState([]);
+  const [selectedInd, setSelectedInd] = React.useState([]);
+
+  const handleGroupChange = event => {
+    setSelectedGroup(event.target.value);
   };
 
-  const handleChangeMultiple = (event) => {
-    const { options } = event.target;
-    const value = [];
-    for (let i = 0, l = options.length; i < l; i += 1) {
-      if (options[i].selected) {
-        value.push(options[i].value);
-      }
-    }
-    setSelectedGroups(value);
+  const handleIndChange = event => {
+    setSelectedInd(event.target.value);
   };
 
   return (
-    <div className="App">
-      <p>This is the main app.</p>
-      <Select
-        multiple
-        value={selectedGroups}
-        onChange={handleChange}
-        input={<Input id="select-multiple" />}
-      >
-        {groupMockValues.map((group) => (
-          <MenuItem key={group} value={group}>
-            {group}
-          </MenuItem>
-        ))}
-      </Select>
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <InputLabel className={classes.label} id="groupSelect">
+          Select the Group(s) this rule will apply to:
+        </InputLabel>
+        <Select
+          className={classes.select}
+          value={selectedGroup}
+          onChange={handleGroupChange}
+          input={<Input id="select-multiple" />}
+          inputProps={{
+            id: "groupSelect"
+          }}
+        >
+          {groupMockValues.map(group => (
+            <MenuItem key={group} value={group}>
+              {group}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
+      <div className={classes.container}>
+        <InputLabel className={classes.label} id="indSelect">
+          Select the Individual(s) this rule will apply to:
+        </InputLabel>
+        <Select
+          className={classes.select}
+          value={selectedInd}
+          onChange={handleIndChange}
+          input={<Input id="select-multiple" />}
+          inputProps={{
+            id: "indSelect"
+          }}
+        >
+          {individualMockValues.map(group => (
+            <MenuItem key={group} value={group}>
+              {group}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
     </div>
   );
 }
