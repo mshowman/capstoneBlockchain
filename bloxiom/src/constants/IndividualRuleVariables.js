ac7a8ce86ed718693ecafce981945cab1d3108c3
MD <mdshowman@gmail.com>
2019-10-22 18:07:11 -0400

    adds mock variable data for groups and individuals

---
+++ bloxiom/src/constants/IndividualRuleVariables.js
@@ -0,0 +1,60 @@
+const mockRuleVariablesForIndividuals = [
+  {
+    name: 'Person 1',
+    variables: [
+      {
+        displayText: 'Spend At One Time',
+        name: 'spendAtOneTime',
+      },
+      {
+        displayText: 'Spend Per Day',
+        name: 'spendPerDay',
+      },
+      {
+        displayText: 'Spend Per Month',
+        name: 'spendPerMonth',
+      },
+    ],
+  },
+  {
+    name: 'Person 2',
+    variables: [
+      {
+        displayText: 'Spend At One Time',
+        name: 'spendAtOneTime',
+      },
+      {
+        displayText: 'Spend Per Day',
+        name: 'spendPerDay',
+      },
+    ],
+  },
+  {
+    name: 'Person 3',
+    variables: [
+      {
+        displayText: 'Spend At One Time',
+        name: 'spendAtOneTime',
+      },
+      {
+        displayText: 'Spend Per Day',
+        name: 'spendPerDay',
+      },
+      {
+        displayText: 'Spend Per Month',
+        name: 'spendPerMonth',
+      },
+    ],
+  },
+  {
+    name: 'Person N',
+    variables: [
+      {
+        displayText: 'Spend Per Month',
+        name: 'spendPerMonth',
+      },
+    ],
+  },
+];
+
+export default mockRuleVariablesForIndividuals;
