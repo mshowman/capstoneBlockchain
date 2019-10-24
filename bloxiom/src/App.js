466786aca2ee5179e9fa451e224177ab3dee22e7
MD Showman <mdshowman@gmail.com>
2019-10-24 12:04:36 -0400

    rules component is complete from a front-end standpoint. no functionality beyond simple logic at this point.

--- bloxiom/src/App.js
+++ bloxiom/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Rule from './components/Rule';
 
-const App = () => <Rule />;
+const App = () => (
+  <div>
+    <h1>This is the placeholder for the Rules Page</h1>
+    <Rule />
+  </div>
+);
 
 export default App;
