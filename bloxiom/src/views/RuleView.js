63eaa233d1c1b26152e568ff1b95ea5608d8220a
MD Showman <mdshowman@gmail.com>
2019-11-10 09:47:37 -0500

    More Layout playing around. Going to refactor Rules component to Dialog for better UX.

--- bloxiom/src/views/RuleView.js
+++ bloxiom/src/views/RuleView.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Rule from '../components/Rule';
 
 const RuleView = () => (
-  <div>
-    <h1>This is the Rules View Page</h1>
-    <span>SideNav?</span>
-    <span><Rule /></span>
-  </div>
+  <>
+    <div style={{ width: '100%' }}>
+      <h1>This is the Rules View Page</h1>
+    </div>
+    <div style={{ display: 'flex', flexDirection: 'row', minHeight: 500 }}>
+      <div style={{ width: 120, backgroundColor: 'rgb(20, 175, 256, 0.5)' }}>SideNav?</div>
+      <div style={{ width: 'calc(100% - 120px)', backgroundColor: 'rgb(20, 175, 256, 0.2)' }}><Rule /></div>
+    </div>
+  </>
 );
 
 export default RuleView;
