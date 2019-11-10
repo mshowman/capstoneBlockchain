b6e728174cc21ea20d88a37849fb183a87be9c3b
MD Showman <mdshowman@gmail.com>
2019-11-10 18:21:09 -0500

    Rules component refactored to dialog. Created SavedRule component. Work on UX for RulesView

---
+++ bloxiom/src/components/Rule/SavedRule.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Snackbar from '@material-ui/core/Snackbar';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import RuleDialog from './RuleDialog';
+
+const useStyles = makeStyles({
+  root: {
+    margin: '10px auto',
+    width: '90%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  ruleText: {
+    fontSize: 14,
+    padding: 10,
+  },
+  buttonGroup: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: 10,
+  },
+  deleteButton: {
+    backgroundColor: 'rgba(255, 0, 0, 0.4)',
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+    '&:hover':
+      {
+        backgroundColor: 'rgba(255, 0, 0, 0.6)',
+      },
+  },
+  editButton: {
+    backgroundColor: 'rgba(0, 255, 0, 0.4)',
+    width: 50,
+    height: 50,
+    '&:hover':
+      {
+        backgroundColor: 'rgba(0, 255, 0, 0.6)',
+      },
+  },
+  saveButton: {
+    backgroundColor: 'rgba(0, 0, 255, 0.4)',
+    width: 50,
+    height: 50,
+    '&:hover':
+      {
+        backgroundColor: 'rgba(0, 0, 255, 0.6)',
+      },
+  },
+});
+
+const SavedRule = ({ info }) => {
+  const classes = useStyles();
+  const [openDeleteCheck, setOpenDeleteCheck] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [openRuleDialog, setOpenRuleDialog] = useState(false);
+
+  const {
+    name, selectedRule, condition, result,
+  } = info;
+
+  function closeRuleDialog() {
+    setOpenRuleDialog(false);
+  }
+
+  return (
+    <Paper className={classes.root}>
+      <Typography className={classes.ruleText}>
+        {`If ${name} ${selectedRule.toLowerCase()} $${condition} or more, ${result.toLowerCase()}.`}
+      </Typography>
+      <div className={classes.buttonGroup}>
+        <IconButton
+          className={classes.editButton}
+          onClick={() => setOpenRuleDialog(true)}
+        >
+          <EditIcon />
+        </IconButton>
+        <IconButton
+          className={classes.deleteButton}
+          onClick={() => setOpenDeleteCheck(true)}
+        >
+          <DeleteForeverIcon />
+        </IconButton>
+      </div>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        key="save-message"
+        message={<span id="message-id">Saved!</span>}
+        autoHideDuration={1000}
+        open={open}
+        onClose={() => setOpen(false)}
+      />
+      <Dialog
+        open={openDeleteCheck}
+        onClose={() => setOpenDeleteCheck(false)}
+      >
+        <DialogTitle>Delete this rule?</DialogTitle>
+        <DialogActions>
+          <Button onClick={() => setOpenDeleteCheck(false)} color="primary">
+            No
+          </Button>
+          <Button onClick={() => setOpenDeleteCheck(false)} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+      { openRuleDialog && (
+        <RuleDialog showDialog closeDialog={closeRuleDialog} info={info} />
+      )}
+    </Paper>
+  );
+};
+
+SavedRule.propTypes = {
+  info: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    selectedRule: PropTypes.string.isRequired,
+    condition: PropTypes.string.isRequired,
+    result: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default SavedRule;
--- bloxiom/src/views/RuleView.js
+++ bloxiom/src/views/RuleView.js
@@ -1,16 +1,100 @@
-import React from 'react';
-import Rule from '../components/Rule';
+import React, { useState } from 'react';
 
-const RuleView = () => (
-  <>
-    <div style={{ width: '100%' }}>
-      <h1>This is the Rules View Page</h1>
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+import SavedRule from '../components/Rule/SavedRule';
+import RuleDialog from '../components/Rule/RuleDialog';
+
+const dummyRule = {
+  groupOrIndividual: 'Individual',
+  name: 'Larry Fitzgerald',
+  selectedRule: 'Spends At One Time',
+  condition: '100',
+  result: 'Sign off needed',
+};
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  mainContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: 'calc(100vh - 64px)',
+    marginTop: 64,
+    padding: 0,
+  },
+  sideNav: {
+    width: 120,
+    backgroundColor: 'rgb(20, 175, 256, 0.5)',
+    padding: 10,
+  },
+  contentWindow: {
+    width: 'calc(100% - 120px)',
+    backgroundColor: 'rgb(20, 175, 256, 0.2)',
+  },
+  addRule: {
+    display: 'flex',
+    flexDirection: 'row',
+    position: 'fixed',
+    bottom: 0,
+    backgroundColor: 'rgb(0, 0, 0, 0.2)',
+    width: 'inherit',
+    padding: 10,
+  },
+  addButton: {
+    margin: '0 auto',
+  },
+});
+
+const RuleView = () => {
+  const classes = useStyles();
+  const [rules, setRules] = useState([1, 2, 1, 2]);
+  const [showDialog, setShowDialog] = useState(false);
+
+  // eslint-disable-next-line no-unused-vars
+  function updateRules(rule) {
+    setRules([...rules, rule]);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+              Rules
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.mainContent}>
+        <div className={classes.sideNav}>SideNav?</div>
+        <div className={classes.contentWindow}>
+          {/* eslint-disable-next-line no-unused-vars,react/no-array-index-key */}
+          {rules.map((rule, i) => <SavedRule key={i} info={dummyRule} />)}
+          <div className={classes.addRule}>
+            <Button
+              className={classes.addButton}
+              onClick={() => setShowDialog(true)}
+              variant="contained"
+              color="primary"
+            >
+              Add New Rule
+            </Button>
+          </div>
+        </div>
+        <div>
+          <RuleDialog showDialog={showDialog} closeDialog={setShowDialog} />
+        </div>
+      </div>
     </div>
-    <div style={{ display: 'flex', flexDirection: 'row', minHeight: 500 }}>
-      <div style={{ width: 120, backgroundColor: 'rgb(20, 175, 256, 0.5)' }}>SideNav?</div>
-      <div style={{ width: 'calc(100% - 120px)', backgroundColor: 'rgb(20, 175, 256, 0.2)' }}><Rule /></div>
-    </div>
-  </>
-);
+  );
+};
 
 export default RuleView;
