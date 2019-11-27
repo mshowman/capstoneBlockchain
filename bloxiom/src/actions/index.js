7aa3ada1b4815beab5c33ea04eeb062a59f36e00
MD <mdshowman@gmail.com>
2019-11-27 17:47:25 -0500

    more fix up

--- bloxiom/src/actions/index.js
+++ bloxiom/src/actions/index.js
@@ -1,48 +1,48 @@
 // RULES ACTIONS
-export const getRules = (userId) => ({
-  type: 'GET_RULES_FOR_USER',
-  userId,
+export const getRules = userId => ({
+  type: "GET_RULES_FOR_USER",
+  userId
 });
 
-export const addRule = (rule) => ({
-  type: 'ADD_RULE',
-  rule,
+export const addRule = rule => ({
+  type: "ADD_RULE",
+  rule
 });
 
-export const deleteRule = (index) => ({
-  type: 'DELETE_RULE',
-  index,
+export const deleteRule = index => ({
+  type: "DELETE_RULE",
+  index
 });
 
 export const editRule = (index, rule) => ({
-  type: 'EDIT_RULE',
+  type: "EDIT_RULE",
   index,
-  rule,
+  rule
 });
 
 // LOGIN ACTIONS
 export const validateCredentials = (username, password, privateKey) => ({
-  type: 'CALL_VALIDATOR',
+  type: "CALL_VALIDATOR",
   username,
   password,
-  privateKey,
+  privateKey
 });
 
 export const ValidationStatuses = {
-  VALIDATING: 'VALIDATING',
-  VALIDATING_SUCCESS: 'VALIDATING_SUCCESS',
-  VALIDATING_ERROR: 'VALIDATING_ERROR',
+  VALIDATING: "VALIDATING",
+  VALIDATING_SUCCESS: "VALIDATING_SUCCESS",
+  VALIDATING_ERROR: "VALIDATING_ERROR"
 };
 
 // USER ACTIONS
 export const retrieveUser = (username, password, privateKey) => ({
-  type: 'GET_USER',
+  type: "GET_USER",
   username,
   password,
-  privateKey,
+  privateKey
 });
 
-export const updateUser = (updatedInfo) => ({
-  type: 'UPDATE_USER_INFO',
-  updatedInfo,
+export const updateUser = updatedInfo => ({
+  type: "UPDATE_USER_INFO",
+  updatedInfo
 });
