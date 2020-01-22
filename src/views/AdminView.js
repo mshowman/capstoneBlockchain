96d5ba568cc1b8665051fad666743b6d762a5af7
MD Showman <mdshowman@gmail.com>
2020-01-21 20:58:02 -0500

    adds placeholder components for Requests and Managing Groups

--- src/views/AdminView.js
+++ src/views/AdminView.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import Requests from '../components/Admin/Requests';
 
-const AdminView = () => <h1>This is the Admin Page</h1>
+const AdminView = () => {
+    const reqdata = [
+        {
+            requester: 'Me',
+            date: '1-21-2020',
+            description: 'Rule info here'
+        },
+        {
+            requester: 'Me',
+            date: '1-21-2020',
+            description: 'Rule info here'
+        },
+        {
+            requester: 'Me',
+            date: '1-21-2020',
+            description: 'Rule info here'
+        },
+        {
+            requester: 'Me',
+            date: '1-21-2020',
+            description: 'Rule info here'
+        },
+    ];
+    return (
+        <div>
+            <Requests requestsArray={reqdata}/>
+            <div>This is the group form</div>
+        </div>
+    );
+}
 
 export default AdminView;
